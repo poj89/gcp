1291d2f7a0bebac8ea91cd23eb51ecfe3b330307
Mario Tejada <mario.tejada@accenture.com>
2022-02-22 18:12:20 -0600

    terraform init works

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
-# This is required for Terraform 0.13+
+
 terraform {
   required_providers {
-    terraform-provider-gcp = {
-      version = "~> 1.0.0"
-      # source  = "C:\Users\mario.tejada\devWorkspace\terraform_updates\GCP\gcptest_02082022\5064-tfmod-cloudsql-google\test\integration\cloudsql\.terraform\plugins\windows_amd64\terraform-provider-gcp.exe"
+    gcp = {
+      # source  = "local/poj89/gcp"
+      version = "~> 1.0.1"
     }
   }
 }
+
+# provider "gcp" {
+#   backup_id = ""
+# }
+
 data "gcp" "backup" {
     instance = google_sql_database_instance.master.name
     most_recent = true
